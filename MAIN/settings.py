333ae9f005a70470427c3198b72bceb2234734dc
kyle calites <kylecalites3@gmail.com>
2026-03-30 12:13:58 +0800

    static folder created

--- MAIN/settings.py
+++ MAIN/settings.py
@@ -79,10 +79,21 @@ WSGI_APPLICATION = 'MAIN.wsgi.application'
 # ==========================
 # Database
 # ==========================
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'HIC_EVALUATION',   # your database name
+        'USER': 'root',                    # your MySQL username
+        'PASSWORD': 'shyle@03092021',       # your MySQL password
+        'HOST': 'localhost',               # or your DB server IP
+        'PORT': '3306',                    # default MySQL port
     }
 }
 
