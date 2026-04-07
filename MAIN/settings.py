eee96ed36fef7e58a0ff50754220c8dacea50ec2
kyle calites <kylecalites3@gmail.com>
2026-04-07 09:55:01 +0800

    allowed host

--- MAIN/settings.py
+++ MAIN/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-dev-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hic-teacher-eval.onrender.com', '127.0.0.1', 'localhost']
 
 # ==========================
 # Email Configuration (Gmail TLS)
